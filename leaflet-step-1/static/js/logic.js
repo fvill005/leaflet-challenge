d20104f8a2b5eac7228d8f200b787daa846c98a1
Filiberto Villegas <filibertovillegas92@gmail.com>
2020-10-02 20:41:40 -0700

    fixed legend issue

--- leaflet-step-1/static/js/logic.js
+++ leaflet-step-1/static/js/logic.js
@@ -3,6 +3,7 @@
 
 var myMap = L.map("map", {
     center: [34.0522, -118.2437],
+
     zoom: 5
 });
 
@@ -14,6 +15,8 @@ L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={
     accessToken: "pk.eyJ1IjoiY2hpYW15YzA5ODciLCJhIjoiY2swdzUxb3I2MGRiMzNpbnliN293OXBteiJ9.at8rk5Trv5oNH1dD2E9EAw"
   }).addTo(myMap);
 
+
+
   //create path for usgs json
   var usgsURL = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
 
@@ -23,7 +26,7 @@ L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={
         return {
           opacity: 1,
           fillOpacity: 1,
-          fillColor: getColor(feature.properties.mag),
+          fillColor: markerColor(feature.properties.mag),
           color: "#00000",
           radius: getRadius(feature.properties.mag),
           stroke: true,
@@ -31,21 +34,20 @@ L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={
         };
       }
       // set different color from magnitude
-        function getColor(magnitude) {
-        switch (true) {
-            case magnitude > 5:
-                return "#ea2c2c";
-              case magnitude > 4:
-                return "#ea822c";
-              case magnitude > 3:
-                return "#ee9c00";
-              case magnitude > 2:
-                return "#eecc00";
-              case magnitude > 1:
-                return "#d4ee00";
-              default:
-                return "#98ee00";
-        }
+      function markerColor(mag) {
+        if (mag <= 1) {
+            return "#ADFF2F";
+        } else if (mag <= 2) {
+            return "#9ACD32";
+        } else if (mag <= 3) {
+            return "#FFFF00";
+        } else if (mag <= 4) {
+            return "#ffd700";
+        } else if (mag <= 5) {
+            return "#FFA500";
+        } else {
+            return "#FF0000";
+        };
       }
       // set radius from magnitude
         function getRadius(magnitude) {
@@ -70,20 +72,21 @@ L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={
         }).addTo(myMap);
       
         // create a legend
-        var legend = L.control({ position: "bottomright" });
-        legend.onAdd = function() {
-            var div = L.DomUtil.create("div", "info legend"), 
-            magnitudeLevels = [0, 1, 2, 3, 4, 5];
-    
-            //div.innerHTML += "<h3>Magnitude</h3>"
-    
-            for (var i = 0; i < magnitudeLevels.length; i++) {
+        var legend = L.control({position: 'bottomright'});
+
+        legend.onAdd = function () {
+        
+            var div = L.DomUtil.create('div', 'info legend'),
+                magnitudes = [0, 1, 2, 3, 4, 5];
+        
+            for (var i = 0; i < magnitudes.length; i++) {
                 div.innerHTML +=
-                    '<i style="background: ' + chooseColor(magnitudeLevels[i] + 1) + '"></i> ' +
-                    magnitudeLevels[i] + (magnitudeLevels[i + 1] ? '&ndash;' + magnitudeLevels[i + 1] + '<br>' : '+');
+                    '<i style="background:' + markerColor(magnitudes[i] + 1) + '"></i> ' + 
+            + magnitudes[i] + (magnitudes[i + 1] ? ' - ' + magnitudes[i + 1] + '<br>' : ' + ');
             }
+        
             return div;
         };
-        // Add Legend to the Map
+        
         legend.addTo(myMap);
   });
